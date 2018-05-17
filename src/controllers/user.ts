9034368ac6dcdbf5b0297d11b6d672da57205e7b
Tilemachos Charalampous <tilemachos.charalampous@gmail.com>
2018-05-17 18:07:55 +0300

    Init extend commit

---
+++ src/controllers/user.ts
@@ -0,0 +1,31 @@
+import { ExpressModuleImpl } from "@app/express/express_module";
+import { PrefixLogger } from "@app/logger/prefix_logger";
+import { DataService } from "@app/service/data_service";
+import { NextFunction, Request, Response } from "express";
+
+class UserController extends ExpressModuleImpl {
+    private dataService: DataService;
+    private logger: PrefixLogger = PrefixLogger.getLogger(UserController.name);
+
+    constructor(dataService: DataService) {
+        super();
+        this.dataService = dataService;
+    }
+
+    public post(req: any, res: Response, next: NextFunction): void {
+        const postLogger: PrefixLogger = this.logger.addPrefix("post");
+        postLogger.info({ requestId: req.id }, "Incoming request");
+        this.dataService.login(req.body.username, req.body.password)
+        .then(() => {
+            this.logger.addPrefix("post").info({requestId: req.id}, "Logged in successfully");
+            res.send();
+        })
+        .catch((err) => {
+            this.logger.addPrefix("post").warn({err, requestId: req.id}, err.message);
+            res.setHeader("WWW-Authenticate", "Basic");
+            res.status(401).send("Invalid username or password");
+        });
+    }
+}
+
+export { UserController };
